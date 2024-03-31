93eb8c44e2cdaa1f1c29d902b9d6e895bafe30f1
KevinMaruthan <157907263+KevinMaruthan@users.noreply.github.com>
2024-03-31 18:20:57 +0100

    learning functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// To define a function prefix the name of the function with `fn`
+// fn function_name(){
+//  // program logic
+// }
+
+// After each exrepssion
+
+//Start of the program
 fn main() {
-    println!("Hello, world!");
+    let mut name = "james";
+    // println!("Hello {name}"); // This is a macro .. Anything ending with `!` is a macro
+    //                           // Macros are not function
+    //                           //  When a macro is used.. a bunch of code that defines the macro would be replaced at that position.
+    //                           //
+    //
+    print(name);
+    let mut name = "kevin";
+    print(name);
+}
+
+fn print(mut str: &str) {
+    str = "sundar";
+    println!("Hello {}", str);
 }
